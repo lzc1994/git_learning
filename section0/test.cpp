c1bf7d8608d4c5b560745a09b11ad49d135839c0
lzc1994 <943935414@qq.com>
2020-08-23 21:52:17 +0800

    test for update file

--- section0/test.cpp
+++ section0/test.cpp
@@ -1,12 +1,10 @@
-// Copyright (c) 2020 by Chrono
+// Copyright (c) 2020 by lzc
 //
 // g++ test.cpp -std=c++11 -o a.out;./a.out
 // g++ test.cpp -std=c++14 -o a.out;./a.out
 // g++ test.cpp -std=c++14 -I../common -o a.out;./a.out
 
 #include <iostream>
-
-//#include "std.hpp"
 
 int main()
 {
